12561cd42ce414ac914c367e46f37068c33923ed
Gabe Troyan <gabriel.troyan@mymail.champlain.edu>
2019-01-25 18:19:51 -0500

    Add project

---
+++ What Home Means To You/Assets/Scripts/Noodle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Noodle : MonoBehaviour
+{
+    public float MovementSpeed = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector2(Input.GetAxis("Horizontal") * Time.deltaTime, Input.GetAxis("Vertical") * Time.deltaTime) * MovementSpeed);
+    }
+
+    public void ScaleShit(float scale)
+    {
+        Time.timeScale = scale;
+    }
+}
