f28de887e6f712f6bb0b06f375e8eb6a5b9ea66e
Gabe Troyan <gabriel.troyan@mymail.champlain.edu>
2019-01-25 20:23:03 -0500

    BAC/time setting screen

---
+++ What Home Means To You/Assets/Scripts/BACAdjustment.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BACAdjustment : MonoBehaviour
+{
+    public List<float> BACList;
+
+    public Text BACSettingText;
+    public Text SecondsText;
+
+    float currentSetting = .40f;
+    int currentIndex = 0;
+    bool hitLastFrame = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BACList.Sort();
+        currentIndex = BACList.FindIndex(x => x == currentSetting);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            if (!hitLastFrame)
+            {
+                currentSetting = BACList[(currentIndex = (currentIndex + BACList.Count + (int)Input.GetAxisRaw("Horizontal")) % BACList.Count)];
+            }
+            hitLastFrame = true;
+        }
+        else
+        {
+            hitLastFrame = false;
+        }
+        BACSettingText.text = currentSetting.ToString("0.00") + " %";
+        SecondsText.text = "(" + (currentSetting / .004).ToString("0") + " seconds)";
+        Data.BAC_MAX = currentSetting;
+    }
+
+    public void LoadScene(string scene)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(scene);
+    }
+}
