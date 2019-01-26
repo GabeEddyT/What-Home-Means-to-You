acc8fe7c4e8c6b645f9bab60917a63e225775461
Gabe Troyan <gabriel.troyan@mymail.champlain.edu>
2019-01-26 07:19:54 -0500

    Change throwing so you can actually hit players

--- What Home Means To You/Assets/Scripts/ThrowableObject.cs
+++ What Home Means To You/Assets/Scripts/ThrowableObject.cs
@@ -7,10 +7,21 @@ public class ThrowableObject : MonoBehaviour
     public float throwSpeed;
     public float spinSpeed;
     public bool grabbable = true;
+    ItemManager []players;
+    
     
     public void Throw(Vector2 startPos, Vector2 direction)
     {
         var rb = GetComponent<Rigidbody2D>();
+        // Figure out who's holding this, and enable collisions for the opposing player.
+        if (transform.parent == players[0].Socket)
+        {
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), players[1].GetComponent<Collider2D>(), false);
+        }
+        else
+        {
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), players[0].GetComponent<Collider2D>(), false);
+        }
         transform.parent = null;
         rb.simulated = true;
         rb.velocity = (direction * throwSpeed);
@@ -20,12 +31,25 @@ public class ThrowableObject : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        players = FindObjectsOfType<ItemManager>();
+        foreach (var item in players)
+        {
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), item.GetComponent<Collider2D>(), true);
+        }
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Disable collisions on players if the object lands or hits someone.
+        foreach (var item in players)
+        {
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), item.GetComponent<Collider2D>(), true);
+        }
     }
 }
