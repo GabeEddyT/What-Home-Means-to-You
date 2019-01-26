3817190b91489429a7c7f971faccea4457f42a76
Gabe Troyan <gabriel.troyan@mymail.champlain.edu>
2019-01-25 20:23:03 -0500

    BAC/time setting screen

---
+++ What Home Means To You/Assets/Scripts/Data.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A static class to store data between scenes without an object.
+/// </summary>
+public static class Data 
+{
+    public static float BAC_MAX = .40f;
+    public static float TURBO_SETTING = 1.0f;
+}
